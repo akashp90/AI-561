4fbe4627295529eb4496832aa9f5fbb330ce4bc4
akash parvatikar <akashp90>
2023-01-27 19:24:20 -0800

    Handle movability between cells

--- Assignment 1/homework.py
+++ Assignment 1/homework.py
@@ -1,3 +1,80 @@
+class Node:
+	children = []
+	x_cord = None
+	y_cord = None
+	elevation = None
+
+	def __init__(self, x, y, elevation):
+		self.x_cord = x
+		self.y_cord = y
+		self.elevation = elevation
+
+def get_surrounding_locations(x_cord, y_cord):
+	north = south = east = west = north_east = north_west = south_west = south_east = None
+	surr = {}
+
+	surr["north"] =  (x_cord, y_cord - 1)
+	surr["south"] = (x_cord, y_cord   + 1)
+	surr["east"] = (x_cord + 1, y_cord)
+	surr["west"] = (x_cord - 1, y_cord)
+	surr["north_east"] = (x_cord + 1, y_cord - 1)
+	surr["north_west"] = (x_cord - 1, y_cord - 1)
+	surr["south_west"] = (x_cord - 1, y_cord + 1)
+	surr["south_east"] = (x_cord + 1, y_cord + 1)
+
+	# Eliminate out of bounds cells
+	for direction in surr:
+	
+		if -1 in surr[direction] or surr[direction][0] == width  or surr[direction][1] == height:
+			surr[direction] = None
+
+	return surr
+
+def is_move_allowed(start_position, end_position):
+	# Stamina check
+	print("Move allowed from " + str(start_position) + "to " + str(end_position) + "??")
+	start_x, start_y = start_position
+	dest_x, dest_y = end_position
+
+	is_dest_tree = True if ski_map[dest_y][dest_x] < 0 else False
+	is_allowed = False
+
+
+	if ski_map[start_y][start_x] >= abs(ski_map[dest_y][dest_x]) and is_dest_tree:
+		# Tree height check
+		is_allowed = True
+	elif ski_map[start_y][start_x] <= ski_map[dest_y][dest_x] and not is_dest_tree:
+		# Stamina check
+		if stamina >= ski_map[dest_y][dest_x] - ski_map[start_y][start_x]:
+			is_allowed = True
+	elif ski_map[start_y][start_x] >= ski_map[dest_y][dest_x] and not is_dest_tree:
+		is_allowed = True
+
+	print(is_allowed)
+	return is_allowed
+
+
+def get_movable_locations(position):
+	
+	x_cord, y_cord = position
+
+	surrounding_locations = get_surrounding_locations(x_cord, y_cord)
+	print("surrounding_locations: ", str(surrounding_locations))
+	movable_locations = []
+
+	for surrounding_location in surrounding_locations:
+		is_movable = is_move_allowed(position, surrounding_locations[surrounding_location])
+
+		if is_movable:
+			movable_locations.append(surrounding_locations[surrounding_location])
+
+	return movable_locations
+
+
+def find_bfs_cost(start, end):
+	unvisited_nodes = []
+	movable_locations = get_movable_locations(start)
+
 def print_map(ski_map):
 	for row in ski_map:
 		print(row)
@@ -35,56 +112,21 @@ for i in range(5, 5 + num_lodges):
 ski_map = [] # Supposed to be an array of arrays
 
 for i in range(0, height):
-	
-	print("Counter: ")
-	print(counter)
 	ski_map_row = file_lines[counter].strip().split(" ")
 	ski_map_row = [int(e) for e in ski_map_row]
-	print("ski map row")
-	print(ski_map_row)
 	ski_map.append(ski_map_row)
 	counter += 1
 
-print("map")
-print_map(ski_map)
 
 
+for lodge in lodge_locs:
+	if algorithm == "BFS":
+		optimal_cost = find_bfs_cost(start_loc, lodge_loc)
 
 
+print(print_map(ski_map))
+print("Stamina: "+ str(stamina))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("Movable locations for 1, 1")
+print(get_movable_locations((1,1)))
 
---
+++ Assignment 1/test.py
@@ -0,0 +1,5 @@
+for i in range(0, 6):
+	for j in range(0, 8):
+		print(str(j) + str(i), end=" ")
+
+	print("\n")
